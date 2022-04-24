e027640f0909b099c7fc319115f9e9a3a5a02ec6
70k11 <wielgus.piotr@gmail.com>
2022-04-24 21:55:27 +0200

    advice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import divider from './images/pattern-divider-desktop.svg';
+import dividerMobile from './images/pattern-divider-mobile.svg';
+import dice from './images/icon-dice.svg';
+
+const url = 'https://api.adviceslip.com/advice';
 
 function App() {
+  const [adviceSlip, setAdviceSlip] = useState({});
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setAdviceSlip(data.slip);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const { id, advice } = adviceSlip;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <article className="article">
+        <h4>ADVICE #{id}</h4>
+        <h1>"{advice}"</h1>
+        <img src={divider} alt="" className="divider-desktop" />
+        <img src={dividerMobile} alt="" className="divider-mobile" />
+      </article>
+      <button onClick={() => fetchAdvice()}>
+        <img src={dice} alt="" />
+      </button>
     </div>
   );
 }
